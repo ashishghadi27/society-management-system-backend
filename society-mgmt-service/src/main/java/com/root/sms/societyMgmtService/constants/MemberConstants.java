4831d72ce212463dfc8076bbccf0434082f24f69
ashishghadi27 <ashishghadi27@gmail.com>
2025-03-02 23:45:18 +0530

    bug fixes

---
+++ society-mgmt-service/src/main/java/com/root/sms/societyMgmtService/constants/MemberConstants.java
@@ -0,0 +1,10 @@
+package com.root.sms.auth_service.constants;
+
+public enum MemberConstants {
+    MEMBER,
+    CHAIRMAN,
+    SECRETARY,
+    TREASURER,
+    ADMIN,
+    APP_OWNER
+}
---
+++ society-mgmt-service/src/main/java/com/root/sms/societyMgmtService/vo/RoomRequest.java
@@ -0,0 +1,4 @@
+package com.root.sms.societyMgmtService.vo;
+
+public class RoomRequest {
+}
